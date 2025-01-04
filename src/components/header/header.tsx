34951cc900f3705372e3a69c4e86a268e1626eb3
danielsalinasmerino <danielsalinas305@gmail.com>
2025-01-04 20:28:16 +0100

    feature: First version of header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import Image from "next/image";
 import React, { ChangeEvent } from "react";
 
 import { useSearchContext } from "@/contexts/search-context";
+
+import styles from "./header.module.css";
+
+const LOGO_PATH = `/images/logos/alt_short_blue.svg`;
 
 const Header = () => {
   const { searchValue, setSearchValue } = useSearchContext();
@@ -12,14 +17,23 @@ const Header = () => {
 
   return (
     <header>
-      <div>
+      <div className={styles.header}>
+        <Image
+          src={LOGO_PATH}
+          alt={`TMDB logo`}
+          width={100}
+          height={100}
+          className={styles.logo}
+          priority
+        />
         <input
           type="text"
           id="searchValue"
           name="searchValue"
           value={searchValue}
           onChange={handleInputChange}
-          placeholder="Search here..."
+          placeholder="Search"
+          className={styles.searcher}
         />
       </div>
     </header>
