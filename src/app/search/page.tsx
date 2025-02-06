58e1451407f9ce4785e04e33336e22939aae5728
Daniel <danielsalinas305@gmail.com>
2025-02-06 14:26:22 +0100

    feature: General improvements

---
+++ src/app/search/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import isEqual from "lodash/isEqual";
+import { useEffect, useState } from "react";
+
+import MovieList from "@/components/movie-list/movie-list";
+import { useSearchMoviesWithCredits } from "@/context/movies/application";
+import { MovieWithCredits } from "@/context/movies/domain";
+import { useDebouncedValue } from "@/utils/hooks";
+import { useAppDispatch, useAppSelector } from "@/utils/react-redux";
+import { setIsLoading } from "@/utils/react-redux/features/moviesSearchSlice";
+
+import styles from "./search.module.css";
+
+export default function Search() {
+  const dispatch = useAppDispatch();
+
+  const searchValue = useAppSelector((state) => state.moviesSearch.searchValue);
+
+  const query = useDebouncedValue(searchValue, 300);
+
+  const [cachedMovies, setCachedMovies] = useState<Array<MovieWithCredits>>([]);
+  const [lastQuery, setLastQuery] = useState<string>("");
+  const [page, setPage] = useState(1);
+
+  const { data, isError, isLoading, refetch } = useSearchMoviesWithCredits({
+    query,
+    page,
+  });
+
+  const maxPages = data?.totalPages || 1;
+
+  const handlePageChange = (newPage: number) =>
+    setPage(Math.min(Math.max(newPage, 1), maxPages));
+
+  useEffect(() => {
+    setPage(1);
+  }, [query]);
+
+  useEffect(() => {
+    dispatch(setIsLoading(isLoading));
+  }, [dispatch, isLoading]);
+
+  useEffect(() => {
+    if (query !== lastQuery) {
+      setLastQuery(query);
+      setCachedMovies([]);
+    }
+  }, [lastQuery, query]);
+
+  useEffect(() => {
+    if (!data?.movies) return;
+
+    setCachedMovies((prev) => {
+      const cachedIds = new Set(prev.map((m) => m.id));
+      const newMovies = data.movies.filter((m) => !cachedIds.has(m.id));
+
+      if (newMovies.length === 0 || isEqual(prev, [...prev, ...newMovies])) {
+        return prev;
+      }
+
+      return [...prev, ...newMovies];
+    });
+  }, [data]);
+
+  return (
+    <div className={styles.page}>
+      {isError && (
+        <div>
+          <p>Error fetching data. Please try again later.</p>
+          <button onClick={() => refetch()}>Retry</button>
+        </div>
+      )}
+
+      <div className={styles.list}>
+        <MovieList movies={cachedMovies} query={query} />
+        {!!cachedMovies.length && (
+          <div>
+            <button
+              onClick={() => handlePageChange(page + 1)}
+              disabled={isLoading}
+            >
+              Show more
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
