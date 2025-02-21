d2b4a4197ed52ee4a5e3183684b101706d9b5b9c
Daniel <danielsalinas305@gmail.com>
2025-02-21 13:26:43 +0100

    refactor: File name refactor

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,4 +1,4 @@
 import axiosTMDBClient from "@/utils/axios/axiosTMDBClient";
-import { handleAxiosError } from "@/utils/axios/hangleAxiosError";
+import { handleAxiosError } from "@/utils/axios/handleAxiosError";
 
 export { axiosTMDBClient, handleAxiosError };
