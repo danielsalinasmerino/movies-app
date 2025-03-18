ee789906840e357906654b6017758ae7d18954e5
Daniel <danielsalinas305@gmail.com>
2025-03-18 13:51:16 +0100

    refactor: Minor typo

--- src/context/persons/application/index.ts
+++ src/context/persons/application/index.ts
@@ -1,4 +1,4 @@
 import { useGetPersonDetails } from "@/context/persons/application/use-get-person-details";
-import { useGetPersonMovieCredits } from "@/context/persons/application/use-get-person-movie-credits";
+import { useGetPersonMoviesCredits } from "@/context/persons/application/use-get-person-movie-credits";
 
-export { useGetPersonDetails, useGetPersonMovieCredits };
+export { useGetPersonDetails, useGetPersonMoviesCredits };
